f0a47fe7840f2e6d9430e861a09e5af185080240
Javid Mamadli <javid.mammadli99@gmail.com>
2026-01-20 22:11:31 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -30,35 +30,35 @@ const topBadges = ["Best Bonus", "Top Pick", "Trending Now"];
 ///
 // Raw casino data without id, rating, and badge
 const casinosRawData: CasinoRawData[] = [
-  {
-    name: "Lizaro",
-    logo: "/lizaro.png",
-    bonus: "350% up to £680 + 200 FS",
-    url: "https://any-site1.com/hhbHzRbC",
-    isMobile: true
-  },
-  {
-    name: "VegasHero",
-    logo: "/VegasHero.png",
-    bonus: "300% up to €500 + 300 FS",
-    url: "https://any-site1.com/sF24tnhr",
-    isMobile: true
-  },
-  {
-    name: "Agent No Wager",
-    logo: "/agentnowager.svg",
-    bonus: "Up to 90 FS + 45% Without Wager!",
-    url: "https://any-site1.com/ht2pbwVK",
-    isMobile: true
-  },
-
-  {
-    name: "HotLoot",
-    logo: "/hotloot.svg",
-    bonus: "250% UP TO €2.500 + 100 FS",
-    url: "https://any-site1.com/YTpqyz6h",
-    isMobile: true
-  },
+  // {
+  //   name: "Lizaro",
+  //   logo: "/lizaro.png",
+  //   bonus: "350% up to £680 + 200 FS",
+  //   url: "https://any-site1.com/hhbHzRbC",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "VegasHero",
+  //   logo: "/VegasHero.png",
+  //   bonus: "300% up to €500 + 300 FS",
+  //   url: "https://any-site1.com/sF24tnhr",
+  //   isMobile: true
+  // },
+  // {
+  //   name: "Agent No Wager",
+  //   logo: "/agentnowager.svg",
+  //   bonus: "Up to 90 FS + 45% Without Wager!",
+  //   url: "https://any-site1.com/ht2pbwVK",
+  //   isMobile: true
+  // },
+  //
+  // {
+  //   name: "HotLoot",
+  //   logo: "/hotloot.svg",
+  //   bonus: "250% UP TO €2.500 + 100 FS",
+  //   url: "https://any-site1.com/YTpqyz6h",
+  //   isMobile: true
+  // },
 
 
   // {
