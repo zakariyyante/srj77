ca29e9abcbf996e373c0d9371a762c1a231e8692
Javid Mamadli <javid.mammadli99@gmail.com>
2026-02-20 20:32:42 +0400

    ..

--- app/data/casinos.ts
+++ app/data/casinos.ts
@@ -49,19 +49,19 @@ const casinosRawData: CasinoRawData[] = [
     isMobile: true
   },
   {
-    name: "HarryCasino",
-    logo: "/harry.svg",
-    bonus: "£1000 BONUS + 100 Free Spins",
-    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
-    votes: 3951,
-    isMobile:true
-  },
-  {
     name: "BetNinja",
     logo: "/BetNinja.png",
     bonus: "£1000 + 100 Free Spins",
     url: "https://topbritcasinos.com/B7W2b6jr?utm_target=betninja",
     votes: 4915,
+    isMobile: true
+  },
+  {
+    name: "Qbet",
+    logo: "/qbet.svg",
+    bonus: "100% UP TO £100 BONUS + 200 FREE SPINS",
+    url: "https://topbritcasinos.com/B7W2b6jr?utm_target=qbet",
+    votes: 3986,
     isMobile: true
   },
   {
@@ -73,14 +73,16 @@ const casinosRawData: CasinoRawData[] = [
     votes: 5872,
     isMobile: true
   },
+
   // {
-  //   name: "Qbet",
-  //   logo: "/qbet.svg",
-  //   bonus: "100% UP TO £100 BONUS + 200 FREE SPINS",
-  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=qbet",
-  //   votes: 3986,
-  //   isMobile: true
+  //   name: "HarryCasino",
+  //   logo: "/harry.svg",
+  //   bonus: "£1000 BONUS + 100 Free Spins",
+  //   url: "https://topbritcasinos.com/B7W2b6jr?utm_target=harry",
+  //   votes: 3951,
+  //   isMobile:true
   // },
+
 
   // {
   //   name: "SpinFin",
